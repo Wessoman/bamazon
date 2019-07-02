cd8ba2bda807345872b0d0c4de1569df97c224b4
Antony Hernandez <wessoman@hotmail.com>
2019-07-01 23:32:38 -0500

    Ver .13

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Bamazon! Bam out the deals today!!
 Release History
 .01-.10 Engine testing and conceptualization
 .11 Initial Readme
-.12 Initial SQL prototype
+.12-13 Initial SQL prototypes
--- bamazonschema.sql
+++ bamazonschema.sql
@@ -13,25 +13,25 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
-VALUES ("Binder Clips (pkg. of 20)", "Office Supplies", "12.00", "50", "0.00"),
-	   ("Coffee Mug", "Household Items", "15.00", "100", "0.00"),
-	   ("100-Piece Puzzle", "Toys and Games", "9.00", "25", "0.00"),
-	   ("Dasani Water Bottles (pkg. of 25)", "Groceries", "32.00", "75", "0.00"),
-	   ("Dyson Vacuum Cleaner", "Household Items", "340.00", "14", "0.00"),
-	   ("Pitch Perfect", "Media", "19.99", "87", "0.00"),
+VALUES ("Col-Erase Non-Photo Blue Pencils, 1 doz.", "Office Supplies", "16.00", "50", "0.00"),
+	   ("Stainless Steel Thermos, 32oz", "Household Items", "22.00", "100", "0.00"),
+	   ("Universal Waite Tarot Deck", "Toys and Games", "12.00", "100", "0.00"),
+	   ("Pocari Sweat 1.5 liter bottles, pack of 8", "Groceries", "80.00", "125", "0.00"),
+	   ("eufy BoostIQ RoboVac 30", "Household Items", "250.00", "20", "0.00"),
+	   ("Neu! LP Box Set, Orange Vinyl", "Media", "200.00", "80", "0.00"),
 	   ("Starbucks K-Cups (pkg. of 20)", "Groceries", "7.50", "111", "0.00"),
 	   ("Black Sharpie", "Office Supplies", "2.50", "200", "0.00"),
 	   ("Scrabble", "Toys and Games", "13.50", "26", "0.00"),
-	   ("Harry Potter T-Shirt", "Apparel", 12.95, 40), 
-    	   ("Stainless Steel Toaster", "Home & Kitchen", 39.99, 200), 
-    	   ("Doctor Who T.A.R.D.I.S. Coffee Mug", "Home & Kitchen", 9.25, 350),
-    	   ("Day of the Dead Leggings", "Apparel", 19.90, 400), 
-    	   ("Organic Matcha Green Tea Powder", "Grocery", 21.05, 1000),
-    	   ("Monster House DVD", "Entertainment", 24.99, 2500),
-    	   ("Graveyard Halloween Decoration Set", "Seasonal", 18.49, 456),
-    	   ("Sweeney Todd Soundtrack CD", "Entertainment", 12.95, 752),
-    	   ("Limited Edition 4th Doctor Scarf", "Apparel", 15.57, 8),
-    	   ("Inflatable 6ft Spider", "Seasonal", 18.83, 204);
+	   ("Harry Potter T-Shirt", "Office Supplies", 12.95, 40), 
+    	   ("Stainless Steel Toaster", "Household Items", 39.99, 200), 
+    	   ("Doctor Who T.A.R.D.I.S. Coffee Mug", "Household Items", 9.25, 350),
+    	   ("Day of the Dead Leggings", "Toys and Games", 19.90, 400), 
+    	   ("Organic Matcha Green Tea Powder", "Groceries", 21.05, 1000),
+    	   ("Monster House DVD", "Media", 24.99, 2500),
+    	   ("Graveyard Halloween Decoration Set", "Electronics", 18.49, 456),
+    	   ("Sweeney Todd Soundtrack CD", "Media", 12.95, 752),
+    	   ("Limited Edition 4th Doctor Scarf", "Office Supplies", 15.57, 8),
+    	   ("Inflatable 6ft Spider", "Electronics", 18.83, 204);
 	   ("End Table", "Household Items", "139.99", "19", "0.00");
 
 CREATE TABLE departments (
@@ -44,7 +44,7 @@ CREATE TABLE departments (
 
 INSERT INTO departments (department_name, over_head_costs, total_sales)
 VALUES ("Office Supplies", "100.50", "0.00"),
-	("Household Items", "60.00", "0.00"),
+	("Electronics", "60.00", "0.00"),
 	("Toys and Games", "50.00", "0.00"),
 	("Groceries", "80.00", "0.00"),
 	("Media", "66.00", "0.00");
